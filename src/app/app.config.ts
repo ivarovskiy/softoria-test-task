95dffe5952ebdfd88af69d77d9890ad785a64c66
Ivarovskiy <ivarovskiy00@gmail.com>
2024-04-09 22:33:36 +0300

    setup env

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,14 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideStore } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideStore(reducers, { metaReducers }),
+  ],
 };
