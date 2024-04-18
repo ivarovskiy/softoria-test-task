3f6c20a1a9e384649a5c3fcc059161fd48c04d17
Ivarovskiy <ivarovskiy00@gmail.com>
2024-04-18 15:22:24 +0300

    created flow

---
+++ src/app/shared/components/day-forecast/day-forecast.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-day-forecast',
+  standalone: true,
+  imports: [],
+  templateUrl: './day-forecast.component.html',
+  styleUrl: './day-forecast.component.scss',
+})
+export class DayForecastComponent {}
---
+++ src/app/shared/components/hightlight/hightlight.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hightlight',
+  standalone: true,
+  imports: [],
+  templateUrl: './hightlight.component.html',
+  styleUrl: './hightlight.component.scss',
+})
+export class HightlightComponent {}
