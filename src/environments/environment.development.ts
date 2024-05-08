a1deb5f9ef49fa461de0b6c12fff26efebd7f71f
ivarovskiy <ivarovskiy00@gmail.com>
2024-05-08 18:21:56 +0300

    'fixed git ignore'

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiHost: 'http://dataservice.accuweather.com',
+  apiKey: 'Wi5ZA9BbGQ0t5m2vsWCJatdpHDK7Owgj',
+  openWeatherApiKey: '237a99b97024872965bf430909ad18b5',
+  openWeatherApiHost: 'http://api.openweathermap.org/data/2.5',
+};
